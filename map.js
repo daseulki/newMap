1ec57f081c517a77ce5f52843e210f363cca112e
daseulki <sisial1002@gmail.com>
2018-10-23 18:07:49 +0900

    commit again

--- map.js
+++ map.js
@@ -4,23 +4,15 @@ let markers = [];
 let circles = [];
 let infowindows = [];
 let polygons = [];
-
-
 let num = 1;
 let geocoder = new daum.maps.services.Geocoder();
 
 const mapContainer = document.getElementById('map'); // 지도를 표시할 div
 const mapOption = {
-
   center: new daum.maps.LatLng(37.48403, 126.894125), // 지도의 중심좌표
   level: 4, // 지도의 확대 레벨
   mapTypeId: daum.maps.MapTypeId.ROADMAP // 지도종류
 };
-
-       center: new daum.maps.LatLng(37.48403, 126.894125), // 지도의 중심좌표
-       level: 4, // 지도의 확대 레벨
-       mapTypeId : daum.maps.MapTypeId.ROADMAP // 지도종류
-   };
 
 // 지도를 생성한다
 const map = new daum.maps.Map(mapContainer, mapOption);
@@ -31,18 +23,11 @@ const mapTypeControl = new daum.maps.MapTypeControl();
 map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
 
 const searchAddrFromCoords = (coords, callback) => {
-
   // 좌표로 행정동 주소 정보를 요청합니다
   geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
 }
 const searchDetailAddrFromCoords = (coords, callback) => {
   // 좌표로 법정동 상세 주소 정보를 요청합니다
-
-// 좌표로 행정동 주소 정보를 요청합니다
-  geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
-}
-const searchDetailAddrFromCoords = (coords, callback) => {
-// 좌표로 법정동 상세 주소 정보를 요청합니다
   geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
 }
 
@@ -61,13 +46,10 @@ const shuffle = () => {
 const mEvent = () => {
 
   daum.maps.event.addListener(map, 'click', (mouseEvent) => {
-
     // 클릭한 위치의 좌표
     //mouseEvent.latLng
 
 
-// 클릭한 위치의 좌표
-//mouseEvent.latLng
 
     searchDetailAddrFromCoords(mouseEvent.latLng, async (result, status) => {
       if (status === daum.maps.services.Status.OK) {
@@ -76,14 +58,11 @@ const mEvent = () => {
 
         let add = '<div class="bAddr">' + detailAddr + '</div>';
         await addMarker(mouseEvent.latLng, add);
-
         console.log(mouseEvent.latLng);
-
         await countMarker();
       }
     });
   });
-
 
   $("#initAll").click(() => {
     console.log("init");
@@ -97,32 +76,17 @@ const mEvent = () => {
     infowindows = [];
     //map.Gb = [];
     num = 1;
-
-  $("#initAll").click(()=>{
-    console.log("init");
-    for (let i in markers){
-      markers[i].setMap(null);
-      circles[i].setMap(null);
-    }
-    markers = [];
-    circles = [];
-
     countMarker();
   });
 }
 
-
 const countMarker = () => {
   let cnt = markers.length;
-  const countMarker = () =>{
-  let cnt = map.Gb.length ;
-
   $('#markerCnt').text(cnt);
   console.log(cnt);
 }
 
 const setCircleColor = (group) => {
-
   if (group === "lora") {
     return "#ffcb11"
   } else if (group === "ble") {
@@ -132,19 +96,6 @@ const setCircleColor = (group) => {
   }
 }
 const getRadius = () => {
-
-  if(group === "lora"){
-    return "#ffcb11"
-  }
-  else if(group === "ble"){
-    return "#56b6fc"
-  }
-  else if(group === "wifi"){
-    return "#fc5656"
-  }
-}
-const getRadius = () =>{
-
   let rad = document.querySelector('input[name="rad"]:checked').value;
   if (rad != "") myradius = rad;
   return rad;
@@ -160,16 +111,6 @@ const addMarker = (position, add) => {
   let circle = new daum.maps.Circle({
     center: position, // 지도의 중심 좌표
     radius: radius, // 원의 반지름 (단위 : m)
-
-  const group = document.getElementById('markerGroup').value;
-  const color = setCircleColor(group);
-  let radius = getRadius()
-  let content = '<div class = "hAddr">' + num + '번째 마커 [ ' +group+ ' ]</div>' + add;
-  // 지도에 원을 표시한다
-  let circle = new daum.maps.Circle({
-    center : position, // 지도의 중심 좌표
-    radius : radius, // 원의 반지름 (단위 : m)
-
     fillOpacity: 0.3, // 채움 불투명도
     strokeWeight: 1, // 선의 두께
     strokeOpacity: 1, // 선 투명도
@@ -178,21 +119,12 @@ const addMarker = (position, add) => {
     strokeColor: color
   });
 
-
   let markerID = group + "_" + shuffle();
   let markerUrl = 'http://image.flaticon.com/icons/svg/787/787535.svg',
     markerSize = new daum.maps.Size(30, 31),
     markerOption = {
       offset: new daum.maps.Point(15, 31)
     };
-
-  let markerID = group +"_"+shuffle();
-  let markerUrl = 'http://image.flaticon.com/icons/svg/787/787535.svg',
-      markerSize = new daum.maps.Size(30,31),
-      markerOption = {
-        offset: new daum.maps.Point(15,31)
-      };
-
   let markerImage = new daum.maps.MarkerImage(markerUrl, markerSize, markerOption);
 
   let marker = new daum.maps.Marker({
@@ -210,7 +142,6 @@ const addMarker = (position, add) => {
 
   marker.setMap(map);
   circle.setMap(map);
-
 
   markers.push(marker);
   markers[markers.length - 1].id = markerID;
@@ -231,32 +162,12 @@ const addMarker = (position, add) => {
       markers.splice(index, 1);
       circles.splice(index, 1);
       infowindows.splice(index, 1);
-
-  markers.push(marker);
-  markers[markers.length-1].id=markerID;
-  circles.push(circle);
-  num += 1;
-  daum.maps.event.addListener(marker, 'click', () => {
-        // 마커 위에 인포윈도우를 표시합니다
-    infowindow.open(map, marker);
-    console.log(infowindow);
-  });
-  daum.maps.event.addListener(marker,'rightclick', (mouseEvent) => {
-    const removeMarker = () =>{
-      let index = markers.map(x=>x.id).indexOf(marker.id);
-      marker.setMap(null);
-      circles[index].setMap(null);
-      infowindow.close();
-      markers.splice(index,1);
-      circles.splice(index,1);
-
     }
 
     removeMarker();
     countMarker();
   });
 }
-
 
 let getGeoData = (level) => {
 		$.getJSON('/geo/'+level+'.geojson', function (geojson) {
